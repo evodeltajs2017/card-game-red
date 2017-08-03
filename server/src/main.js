5cff1b502f480e224a56855265a3091745395ee6
Alex Copindean <akitektuo@yahoo.com>
2017-08-03 11:36:34 +0300

    Added getAllCardTypes end point

--- server/src/main.js
+++ server/src/main.js
@@ -8,6 +8,10 @@ const SampleRoute = require("./routes/sample/SampleRoute");
 const sampleRoute = new SampleRoute(app);
 sampleRoute.initialize();
 
+const ViewCardTypes = require("./routes/view-card-types/GetAllCardTypesRoute");
+const viewCardTypes = new ViewCardTypes(app);
+viewCardTypes.initialize();
+
 app.listen(3000, function() {
 	console.log("Example app listening on port 3000!");
 });
