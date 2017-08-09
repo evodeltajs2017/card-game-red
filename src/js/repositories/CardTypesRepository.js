b65424b3d736582d9fa05010403b3fd418001a00
Akitektuo <alexcopindean@yahoo.com>
2017-08-09 10:11:33 +0300

    Removed console logs

--- src/js/repositories/CardTypesRepository.js
+++ src/js/repositories/CardTypesRepository.js
@@ -6,12 +6,11 @@ class CardTypesRepository {
 	getCardTypes(pageIndex, searchName) {
 		return new Promise((resolve, reject) => {
 			const xhr = new XMLHttpRequest();
-			let query = `searchName=${searchName}`;
+			let query = `searchName=${searchName}&`;
 			if (searchName === undefined) {
 				query = "";
 			}
-			console.log(`http://localhost:3000/view-card-types?${query}&pageIndex=${(pageIndex - 1) * 10}`);
-			xhr.open("GET", `http://localhost:3000/view-card-types?${query}&pageIndex=${(pageIndex - 1) * 10}`, true);
+			xhr.open("GET", `http://localhost:3000/view-card-types?${query}pageIndex=${(pageIndex - 1) * 10}`, true);
 			xhr.onload = () => { resolve(JSON.parse(xhr.responseText)); };
 			xhr.onerror = () => { reject(xhr.statusText); };
 			xhr.send();
