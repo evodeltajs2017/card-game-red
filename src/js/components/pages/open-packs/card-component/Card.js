1061e31ff54494a7bd6ff2df6f2baa8c8b5ce864
Carausu Catrinel <tipitza@gmail.com>
2017-08-10 16:34:33 +0300

    Updated card design

--- src/js/components/pages/open-packs/card-component/Card.js
+++ src/js/components/pages/open-packs/card-component/Card.js
@@ -7,11 +7,11 @@ class Card{
     initialize() {
         const div = document.createElement("div");
         div.className = " centered-card-pack card";
-        div.innerHTML = `<p>Name: ${this.cardData.Name}</p>
-                            <p>Cost: ${this.cardData.Cost}</p>
-                            <p>Damage: ${this.cardData.Damage}</p>
-                            <p>Health: ${this.cardData.Health}</p>
-                            <p>ImageId: ${this.cardData.ImageIdentifier}</p>`;
+        div.innerHTML = `<p class="card-cost">${this.cardData.Cost}</p>
+                            <p class="card-name">${this.cardData.Name}</p>
+                            <p class="card-damage">${this.cardData.Damage}</p>
+                            <p class="card-health" >${this.cardData.Health}</p>
+                            <p class="card-image">${this.cardData.ImageIdentifier}</p>`;
 
         this.container.appendChild(div);
         this.domElement = div;
